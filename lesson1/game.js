86fbded705219d3e706ae7da574c243cabe21e34
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-04-25 13:05:06 +0300

    AnimatedSprite demo

--- lesson1/game.js
+++ lesson1/game.js
@@ -1,24 +1,52 @@
 const app = new PIXI.Application({
 	width: 720,
-	height: 1280
+	height: 1280,
+    autoStart: false
 });
 
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 container.appendChild(app.view);
 
 app.ticker.add(update);
-app.loader.add("ship","./../assets/ship.png").load(load);
+app.loader.baseUrl = '../assets';
+app.loader.add('ship_straight', 'ship_straight.png')
+    .add('ship_turn', 'ship_turn.png')
+    .load(init);
 
-var ship;
+function init() {
+    let ship = createShip();
+    ship.position.set(720 / 2, 1000);
 
-function load(loader) {
-    ship = new PIXI.Sprite(loader.resources["ship"].texture);
-    ship.anchor.set(.5);
-    ship.position.x = 720 / 2;
-    ship.position.y = 1200;
-    app.stage.addChild(ship);
+    let ship2 = createShip();
+    ship2.position.set(720 / 2 - 200, 800);
 
+    app.ticker.start();
 }
 
-function update() {
+function createShip() {
+    let resources = app.loader.resources;
+
+    let texLeft = resources['ship_turn'].texture;
+    let texForward = resources['ship_straight'].texture;
+    let texRight = new PIXI.Texture(texLeft.baseTexture, texLeft.frame);
+    texRight.rotate = 12;
+    let textures = [texLeft, texForward, texRight, texForward];
+
+    ship = new PIXI.AnimatedSprite(textures);
+    ship.play();
+    ship.animationSpeed = 0.05;
+    app.stage.addChild(ship);
+
+    ship.scale.set(2);
+    ship.anchor.set(0.5);
+
+    return ship;
+}
+
+function update(delta) {
+    // if autoUpdate is false
+
+    // if (ship.playing) {
+    //     ship.update(delta);
+    // }
 }
