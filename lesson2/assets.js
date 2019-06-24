da654447c7729d733d9b4cba78f184b3f9f56505
Konstantin <rondo.devil@gmail.com>
2019-06-24 13:26:09 +0300

    Initial game stage

--- lesson2/assets.js
+++ lesson2/assets.js
@@ -3,13 +3,22 @@ import Config from "./config.js";
 export default {
 	baseUrl: Config.assetsBaseUrl,
 	assetList: [
-		{
-			key: "bg_tiled_layer1",
-			url: "bg_tiled_layer1.png"
-		},
-		{
-			key: "bg_tiled_layer2",
-			url: "bg_tiled_layer2_stars.png"
-		}
+        //background
+		{ key: "bg_tiled_layer1", url: "bg_tiled_layer1.png" },
+		{ key: "bg_tiled_layer2", url: "bg_tiled_layer2_stars.png" },
+    
+        
+        //ship
+        { key: "ship_straight", url: "ship_straight.png" },
+        { key: "ship_turn", url: "ship_turn.png" },
+        
+        //enemies
+        { key: "snake", url: "snake.png" },
+        { key: "enemy", url: "48_enemy.png" },
+
+        //turel
+        { key: "turel_base", url: "rail_gun_base.png" },
+        { key: "turel_gun", url: "rail_gun.png" }
+
 	]
 };
---
+++ lesson2/stages/game.js
@@ -0,0 +1,19 @@
+export default class Game extends PIXI.Container {
+    /**
+     * Create game stage
+     * @param {PIXI.Application} app 
+     */
+    constructor(app) {
+        super();
+
+        this.app = app;
+    }
+
+    /**
+     * Update stage 
+     * @param {number} delta 
+     */
+    update(delta) {
+
+    }
+}
