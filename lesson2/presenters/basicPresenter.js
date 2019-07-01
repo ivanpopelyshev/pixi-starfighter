e5a9f60e50e9816c2f2fed8c7fe853126fe1a2a4
Konstantin <rondo.devil@gmail.com>
2019-07-01 17:52:48 +0300

    Remove deprications

--- lesson2/presenters/basicPresenter.js
+++ lesson2/presenters/basicPresenter.js
@@ -3,29 +3,23 @@ import ObjectPool from "../core/objectPool.js";
 export default class BasicPresenter {
 	/**
 	 * Basic presenter
-	 * @param {PIXI.Container} root 
-	 * @param {PIXI.IResourceDictionary} resourceDict 
+	 * @param {PIXI.Container} root
+	 * @param {PIXI.IResourceDictionary} resourceDict
 	 * @param {*} runtime,
 	 * @param {Array} allowedTags
 	 */
 	constructor(root, resourceDict, runtime, allowedTags) {
-
 		this.selector = {};
-		if(!Array.isArray(allowedTags) || allowedTags.length == 0 ) {
+		if (!Array.isArray(allowedTags) || allowedTags.length == 0) {
 			throw new Error("allowedTags must be Array and can't be empty!");
 		}
-		allowedTags.forEach((e)=> this.selector[e] = true );
+		allowedTags.forEach(e => (this.selector[e] = true));
 
 		this.root = root;
 		this.runtime = runtime;
 		this.res = resourceDict;
-		
-		this._pool = new ObjectPool(
-			this.createView.bind(this),
-			this.initView.bind(this),
-			this.resetView.bind(this),
-			0
-		);
+
+		this._pool = new ObjectPool(this.createView.bind(this), this.initView.bind(this), this.resetView.bind(this), 0);
 
 		/**
 		 * Actual view list. Refresh after presenting or pair
@@ -34,55 +28,19 @@ export default class BasicPresenter {
 		this.actualViews = this._pool._used;
 	}
 
-	
-	/**
-	 * Pair models and presenter
-	 * @public
-	 * @deprecated Use runtime.Add instead
-	 * @param {Array} modeles
-	 */
-	
-	pair(modeles) {
-		if (!modeles) {
-			return;
-		}
-		this.add(...modeles);
-	}
-
-	/**
-	 * Add model to presenter
-	 * @deprecated Use Runtime.add instead
-	 * @param  {...any} model Model for pairing
-	 */
-	add(...model) {
-		this.runtime.add(...model);
-		this.actualViews = this._pool._used;
-	}
-
-	/**
-	 * Remove model from presenter
-	 * @deprecated Use Runtime.remove instead
-	 * @param {*} idOrModel 
-	 */
-	remove(idOrModel) {
-		this.runtime.remove(idOrModel);
-	}
-
 	/**
 	 * @public
-	 * @deprecated
 	 * Synchronize models and views
 	 * @param {any} args Any arguments
 	 */
 	present(model, args = undefined) {
-
-		if(!this.selector[model.tag]) {
+		if (!this.selector[model.tag]) {
 			return;
 		}
 
 		let view = model.view;
 
-		if(!view) {
+		if (!view) {
 			view = this._pool.get();
 			model.view = view;
 			view.model = model;
@@ -96,12 +54,14 @@ export default class BasicPresenter {
 	 */
 	flush() {
 		const count = this._pool.usedSize;
-		for(let i = count - 1; i >= 0; i --) {
+		for (let i = count - 1; i >= 0; i--) {
 			const view = this._pool._used[i];
-			if(view.model == undefined || view.model.killMe) {
+			if (view.model == undefined || view.model.killMe) {
 				this._pool.release(view);
 			}
 		}
+		
+		this.actualViews = this._pool._used;
 	}
 
 	/**
@@ -113,15 +73,14 @@ export default class BasicPresenter {
 	 */
 	presentPair(view, model, args) {
 		let { position } = model;
-		let {delta = 1} = args;
-		
+		let { delta = 1 } = args;
+
 		model.position.x += model.vel.x * delta;
 		model.position.y += model.vel.y * delta;
 		view.position.set(position.x, position.y);
 		view.tint = model.tint || 0xffffff;
 		view.scale.set(model.size || 1);
 	}
-
 
 	/**
 	 * @protected
